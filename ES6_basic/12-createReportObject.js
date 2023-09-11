617bacadc0e1aac3014b86fe7919792efaa7b29f
Ghassen69 <ghassenayari2000@gmail.com>
2023-09-11 18:43:39 +0200

    report

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+  return obj;
+}
+
+export default createReportObject;
